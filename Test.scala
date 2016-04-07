dc302954b17c1b0489551e68d9bd2392b543889f
Tj1014 <tj1014@yahoo.com>
2016-04-06 23:36:42 -0400

    Update Test.scala

--- Test.scala
+++ Test.scala
@@ -9,3 +9,18 @@ object MyModule {
   def main(args: Array[String]) =
     println(formatAbs (-42))
 }
+object Factorial {
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+    go (n, 1)
+  }
+}
+object Result {
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg format (name, n, f(n))
+  }
+}
